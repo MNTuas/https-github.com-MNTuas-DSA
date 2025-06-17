648567d0699cd73c8370c74aa222a5bbce0d86c1
MinTuan <146855099+MNTuas@users.noreply.github.com>
2025-06-17 12:13:52 +0700

    Time: 1 ms (98.46%), Space: 42.2 MB (36.98%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -2,22 +2,15 @@ public class Solution {
     public bool IsValid(string s) {
         Stack<char> stack = new Stack<char>();
 
-        foreach( char c in s){
-            if(c == '(' || c == '{' || c == '['){
-                stack.Push(c);
+        foreach(char m in s){
+            if(m == '(' || m == '[' || m == '{'){
+                stack.Push(m);
             }else{
-                
                 if(stack.Count == 0) return false;
-                
                 char open = stack.Pop();
-                
-                if (c == ')' && open != '(' || 
-                
-                    c == ']' && open != '[' || 
-                
-                    c == '}' && open != '{' 
-                    ) 
+                if(m == ')' && open != '(' || m == '}' && open != '{' || m == ']' && open != '[')
                 return false;
+
             }
         }
         return stack.Count == 0;
