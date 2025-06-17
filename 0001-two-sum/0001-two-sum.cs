9de5959ce4c7ed032de713bfe6bb600131877cc4
MinTuan <146855099+MNTuas@users.noreply.github.com>
2025-06-17 16:23:04 +0700

    Time: 1 ms (98.47%), Space: 48.7 MB (64.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,11 +1,15 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
         for(int i = 0; i < nums.Length; i++){
-            for(int j = i + 1; j < nums.Length; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] {i,j};
-                }
+            int remain = target - nums[i];
+
+            if(map.ContainsKey(remain)){
+                return new int[] {map[remain], i};
             }
+
+            map[nums[i]] = i;
         }
         return new int[0];
     }
